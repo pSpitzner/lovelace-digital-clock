b8b16d0a2fe22f6995277fc5e47c917cb47426c4
pSpitzner <paul.spitzner@ds.mpg.de>
2024-05-27 02:08:11 +0200

    fixed font spacing problem and added coloring by time.

--- src/RetroClock.ts
+++ src/RetroClock.ts
@@ -25,78 +25,113 @@ console.info(
 
 @customElement('retro-clock')
 export class RetroClock extends LitElement {
-    @property({attribute: false}) public hass!: HomeAssistant;
-    @state() private _firstLine = '';
-    @state() private _secondLine = '';
-    @state() private _config?: IRetroClockConfig;
-    @state() private _interval = 1000;
-    private _intervalId?: number;
+  @property({ attribute: false }) public hass!: HomeAssistant;
+  @state() private _firstLine = '';
+  @state() private _secondLine = '';
+  @state() private _config?: IRetroClockConfig;
+  @state() private _interval = 1000;
+  private _intervalId?: number;
 
-    public setConfig(config: IRetroClockConfig): void {
-        this._config = {...config};
-        if (this._config.timeFormat)
-            this._config.firstLineFormat = this._config.timeFormat;
-        if (this._config.dateFormat)
-            this._config.secondLineFormat = this._config.dateFormat;
-        if (this._config.interval !== this._interval)
-            this._interval = this._config.interval ?? 1000;
+  public setConfig(config: IRetroClockConfig): void {
+    this._config = { ...config };
+    if (this._config.timeFormat) this._config.firstLineFormat = this._config.timeFormat;
+    if (this._config.dateFormat) this._config.secondLineFormat = this._config.dateFormat;
+    if (this._config.interval !== this._interval) this._interval = this._config.interval ?? 1000;
+  }
+
+  protected shouldUpdate(changedProps: PropertyValues): boolean {
+    return (
+      changedProps.has('_firstLine') ||
+      changedProps.has('_secondLine') ||
+      changedProps.has('_config') ||
+      changedProps.has('hass')
+    );
+  }
+
+  public async getCardSize(): Promise<number> {
+    return 3;
+  }
+
+  protected updated(changedProperties: PropertyValues): void {
+    super.updated(changedProperties);
+
+    if (changedProperties.has('_interval')) {
+      this._stopInterval();
+      this._startInterval();
+    }
+    if (changedProperties.has('_config')) this._updateDateTime();
+  }
+
+  public connectedCallback(): void {
+    super.connectedCallback();
+    this._startInterval();
+  }
+
+  private _startInterval(): void {
+    if (this._intervalId) return;
+
+    this._intervalId = window.setInterval(this._updateDateTime.bind(this), this._interval);
+  }
+
+  private _stopInterval(): void {
+    if (!this._intervalId) return;
+    window.clearInterval(this._intervalId);
+    this._intervalId = undefined;
+  }
+
+
+
+
+  private _timeToRgb(): number[] {
+    const now = DateTime.local();
+    const currentTime = now.hour * 3600 + now.minute * 60 + now.second;
+
+    // Define your key times and their colors here.
+    // Times are in seconds since midnight.
+    const keyTimes = [
+      { time: 0 * 3600, color: hexToRgb('#F18C8E') },
+      { time: 4 * 3600, color: hexToRgb('#F0B7A4') },
+      { time: 7 * 3600, color: hexToRgb('#F1D1B5') },
+      { time: 12 * 3600, color: hexToRgb('#FFF') },
+      { time: 18 * 3600, color: hexToRgb('#8CB4C5') },
+      { time: 20 * 3600, color: hexToRgb('#548EA7') },
+      { time: 22 * 3600, color: hexToRgb('#998BA8') },
+    ];
+
+    // Find the two key times that the current time falls between.
+    let i = 0;
+    while (currentTime >= keyTimes[i + 1].time) {
+      i++;
     }
 
-    protected shouldUpdate(changedProps: PropertyValues): boolean {
-        return changedProps.has('_firstLine') || changedProps.has('_secondLine') || changedProps.has('_config') || changedProps.has('hass');
-    }
+    // Interpolate between the colors of the two key times.
+    const t = (currentTime - keyTimes[i].time) / (keyTimes[i + 1].time - keyTimes[i].time);
+    const color = keyTimes[i].color.map((start, j) => Math.floor(start + t * (keyTimes[i + 1].color[j] - start)));
 
-    public async getCardSize(): Promise<number> {
-        return 3;
-    }
+    return [color[0], color[1], color[2]];
+  }
 
-    protected updated(changedProperties: PropertyValues): void {
-        super.updated(changedProperties);
+  private _formatTime(dateString: string, blinkDividers = false): string {
 
-        if (changedProperties.has('_interval')) {
-            this._stopInterval();
-            this._startInterval();
-        }
-        if (changedProperties.has('_config'))
-            this._updateDateTime();
-    }
+    let timeDiv = dateString;
+    let placeholder = dateString.replace(/\d/g, '8').replace(/[a-zA-Z]/g, 'B');
+    // we want the separator in the less wide font, thus give it a class
+    if (blinkDividers) timeDiv = timeDiv.replace(/:/g, '<span class="blink separator">:</span>');
+    else timeDiv = timeDiv.replace(/:/g, '<span class="separator">:</span>');
+    placeholder = placeholder.replace(/:/g, '<span class="separator">:</span>');
 
-    public connectedCallback(): void {
-        super.connectedCallback();
-        this._startInterval();
-    }
+    timeDiv = `<div class="foreground">${timeDiv}</div>`;
+    placeholder = `<div class="placeholder">${placeholder}</div>`;
 
-    private _startInterval(): void {
-        if (this._intervalId)
-            return;
+    return `${timeDiv}${placeholder}`;
+  }
 
-        this._intervalId = window.setInterval(this._updateDateTime.bind(this), this._interval);
-    }
+  private async _updateDateTime(): Promise<void> {
+    const timeZone = this._config?.timeZone ?? this.hass?.config?.time_zone;
+    const locale = this._config?.locale ?? this.hass?.locale?.language;
 
-    private _stopInterval(): void {
-        if (!this._intervalId)
-            return;
-        window.clearInterval(this._intervalId);
-        this._intervalId = undefined;
-    }
-
-    private _formatTime(dateString: string, blinkDividers=false): string {
-        const placeholder = dateString.replace(/\d/g, '8').replace(/[a-zA-Z]/g, 'B');
-        const placeholderDiv = `<div class="placeholder">${placeholder}</div>`;
-        let timeDiv = `<div class="foreground">${dateString}</div>`;
-
-        if (blinkDividers)
-            timeDiv = timeDiv.replace(/:/g, '<span class="blink">:</span>');
-
-        return `${timeDiv}${placeholderDiv}`;
-    }
-
-    private async _updateDateTime(): Promise<void> {
-        const timeZone = this._config?.timeZone ?? this.hass?.config?.time_zone;
-        const locale = this._config?.locale ?? this.hass?.locale?.language;
-
-        let dateTime: DateTime = DateTime.local();
-        /* if (!this._config?.useHATime) {
+    let dateTime: DateTime = DateTime.local();
+    /* if (!this._config?.useHATime) {
             dateTime = DateTime.local();
         } else {
             dateTime = DateTime.fromSeconds(await new Promise<number>((resolve) => {
@@ -107,107 +142,119 @@ export class RetroClock extends LitElement {
             }));
         } */
 
-        if (timeZone)
-            dateTime = dateTime.setZone(timeZone);
-        if (locale)
-            dateTime = dateTime.setLocale(locale);
+    if (timeZone) dateTime = dateTime.setZone(timeZone);
+    if (locale) dateTime = dateTime.setLocale(locale);
 
-        let firstLine: string;
-        let secondLine: string;
+    let firstLine: string;
+    let secondLine: string;
 
-        if (typeof this._config?.firstLineFormat === 'string')
-            firstLine = dateTime.toFormat(this._config.firstLineFormat);
-        else
-            firstLine = dateTime.toLocaleString(this._config?.firstLineFormat ?? {hour: '2-digit', minute: '2-digit'});
+    if (typeof this._config?.firstLineFormat === 'string') firstLine = dateTime.toFormat(this._config.firstLineFormat);
+    else firstLine = dateTime.toLocaleString(this._config?.firstLineFormat ?? { hour: '2-digit', minute: '2-digit' });
 
-        if (typeof this._config?.secondLineFormat === 'string')
-            secondLine = dateTime.toFormat(this._config.secondLineFormat);
-        else
-            secondLine = dateTime.toLocaleString(this._config?.secondLineFormat ?? {weekday: 'short', day: '2-digit', month: 'short'});
+    if (typeof this._config?.secondLineFormat === 'string')
+      secondLine = dateTime.toFormat(this._config.secondLineFormat);
+    else
+      secondLine = dateTime.toLocaleString(
+        this._config?.secondLineFormat ?? { weekday: 'short', day: '2-digit', month: 'short' },
+      );
 
-        firstLine = this._formatTime(firstLine, this._config?.blinkDividers);
-        secondLine = this._formatTime(secondLine);
+    firstLine = this._formatTime(firstLine, this._config?.blinkDividers);
+    secondLine = this._formatTime(secondLine);
 
-        if (firstLine !== this._firstLine)
-            this._firstLine = firstLine;
-        if (secondLine !== this._secondLine)
-            this._secondLine = secondLine;
-    }
+    if (firstLine !== this._firstLine) this._firstLine = firstLine;
+    if (secondLine !== this._secondLine) this._secondLine = secondLine;
+  }
 
-    public disconnectedCallback(): void {
-        this._stopInterval();
-        super.disconnectedCallback();
-    }
+  public disconnectedCallback(): void {
+    this._stopInterval();
+    super.disconnectedCallback();
+  }
 
-    protected render(): TemplateResult | void {
-        return html`
-          <ha-card>
-            <div class="line first-line">
-              ${unsafeHTML(this._firstLine)}
-            </div>
-            <div class="line second-line">
-              ${unsafeHTML(this._secondLine)}
-            </div>
-          </ha-card>
-        `;
-    }
+  protected render(): TemplateResult | void {
+    const color = this._timeToRgb();
+    return html`
+      <ha-card style="--text-color: rgb(${color}); --glow-color: rgba(${color}, 0.4);">
+        <div class="line first-line">
+          ${unsafeHTML(this._firstLine)}
+        </div>
+        <div class="line second-line">
+          ${unsafeHTML(this._secondLine)}
+        </div>
+      </ha-card>
+    `;
+  }
+
+  static get styles(): CSSResult {
+    // we should probably integrate the fonts somehow:
+    // https://community.home-assistant.io/t/use-ttf-in-lovelace/143495/33
+    return css`
+      ha-card {
+        padding-right: 8px;
+        text-align: right;
+        font-family: 'digital-7 (mono)', sans-serif;
+      }
+
+      .line {
+        position: relative;
+      }
+
+      .line .foreground {
+        display: inline-block;
+        position: relative;
+        top: 0;
+        left: 0;
+        right: 0;
+        z-index: 100;
+        color: var(--text-color);
+        text-shadow: 0 0 5px var(--glow-color), 0 0 10px var(--glow-color), 0 0 15px var(--glow-color),
+          0 0 20px var(--glow-color);
+      }
+
+      .line .placeholder {
+        display: inline-block;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        z-index: 50;
+        opacity: 0.15;
+        color: var(--text-color);
+      }
+
+      .line .separator {
+        font-family: 'digital-7', sans-serif;
+      }
+
+      .first-line {
+        font-size: 2em;
+        line-height: 1em;
+      }
+
+      .second-line {
+        font-size: 1em;
+        line-height: 1em;
+      }
+
+      @keyframes blink {
+        0%,
+        49% {
+          opacity: 1;
+        }
+        50%,
+        100% {
+          opacity: 0;
+        }
+      }
+
+      .blink {
+        animation: blink 2s steps(1) infinite;
+      }
+    `;
+  }
+}
 
 
-    static get styles(): CSSResult {
-        // we should probably integrate the fonts somehow:
-        // https://community.home-assistant.io/t/use-ttf-in-lovelace/143495/33
-        return css`
-          ha-card {
-            padding-right: 8px;
-            text-align: right;
-            font-family: 'digital-7', sans-serif;
-          }
-
-          .line {
-            position: relative;
-          }
-          .line .foreground {
-            display: inline-block;
-            position: relative;
-            top: 0;
-            left: 0;
-            right: 0;
-            z-index: 100;
-          }
-          .line .placeholder {
-            display: inline-block;
-            position: absolute;
-            top: 0;
-            left: 0;
-            right: 0;
-            z-index: 50;
-            opacity: 0.1;
-          }
-
-          .first-line {
-            font-size: 2em;
-            line-height: 1em;
-          }
-
-          .second-line {
-            font-size: 1em;
-            line-height: 1em;
-          }
-
-          @keyframes blink {
-            0%,
-            49% {
-              opacity: 1;
-            }
-            50%,
-            100% {
-              opacity: 0;
-            }
-          }
-
-          .blink {
-            animation: blink 2s steps(1) infinite;
-          }
-        `;
-    }
+function hexToRgb(hex: string): number[] {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)] : [0, 0, 0];
 }
